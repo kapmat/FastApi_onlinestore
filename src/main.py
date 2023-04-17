19b577dfc3d36d32f6d761e9009b51367ad6a69f
kapmat <kapitonov.matvey@gmail.com>
2023-04-17 17:29:50 +0300

    products_crud v.1

--- src/main.py
+++ src/main.py
@@ -5,26 +5,14 @@ from auth.schemas import UserRead, UserCreate
 
 from products.router import router as router_products
 from auth.router import router as router_auth
+
 app = FastAPI(
     title="Social_network"
 )
 
-
-# app.include_router(
-#     fastapi_users.get_auth_router(auth_backend),
-#     prefix="/auth/jwt",
-#     tags=["Auth"],
-# )
-#
-# app.include_router(
-#     fastapi_users.get_register_router(UserRead, UserCreate),
-#     prefix="/auth",
-#     tags=["Auth"],
-# )
-
 app.include_router(router_auth)
 app.include_router(router_products)
-# current_user = fastapi_users.current_user()
+
 
 
 
--- src/products/router.py
+++ src/products/router.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy import select, insert
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select, insert, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from auth.models import User
@@ -15,31 +15,93 @@ router = APIRouter(
 
 @router.post("/")
 async def add_product(new_product: AddProduct, session: AsyncSession = Depends(get_async_session)):
-    stmt = insert(Product).values(**new_product.dict())
-    await session.execute(stmt)
-    await session.commit()
-    return {"status": "success"}
+    try:
+        stmt = insert(Product).values(**new_product.dict())
+        await session.execute(stmt)
+        await session.commit()
+        return {
+            201: "success",
+            "data": None,
+            "details": "New product successfully added!"
+        }
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": ex
+        })
 
 
 @router.get("/")
 async def get_all_products(session: AsyncSession = Depends(get_async_session)):
-    query = select(Product)
-    result = await session.execute(query)
-    return result.scalars().all()
+    try:
+        query = select(Product)
+        result = await session.execute(query)
+        return {
+            200: "success",
+            "data": result.scalars().all(),
+            "details": None
+        }
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": ex
+        })
 
 
-@router.get("/categories")
-async def get_all_categories(session: AsyncSession = Depends(get_async_session)):
-    query = select(Category)
-    result = await session.execute(query)
-    return result.scalars().all()
+@router.delete("/")
+async def drop_product(drop_product_id: int, session: AsyncSession = Depends(get_async_session)):
+    try:
+        stmt = delete(Product).where(Product.product_id == drop_product_id)
+        await session.execute(stmt)
+        await session.commit()
+        return {
+            200: "success",
+            "data": None,
+            "details": "The product has been successfully removed!"
+        }
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": ex
+        })
 
 
-@router.get("/{product_id}")
-async def get_product(product_id: int, session: AsyncSession = Depends(get_async_session)):
-    query = select(Product).where(Product.product_id == product_id)
-    result = await session.execute(query)
-    return result.scalars().all()
+@router.put("/")
+async def add_product(put_product_id: int, put_product: AddProduct, session: AsyncSession = Depends(get_async_session)):
+    try:
+        stmt = update(Product).where(Product.product_id == put_product_id).values(**put_product.dict())
+        await session.execute(stmt)
+        await session.commit()
+        return {
+            200: "success",
+            "data": None,
+            "details": "The product has been successfully modified!"
+        }
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": ex
+        })
+
+#
+# @router.get("/categories")
+# async def get_all_categories(session: AsyncSession = Depends(get_async_session)):
+#     query = select(Category)
+#     result = await session.execute(query)
+#     return result.scalars().all()
+#
+#
+# @router.get("/{product_id}")
+# async def get_product(product_id: int, session: AsyncSession = Depends(get_async_session)):
+#     query = select(Product).where(Product.product_id == product_id)
+#     result = await session.execute(query)
+#     return result.scalars().all()
+
+
 
 
 
